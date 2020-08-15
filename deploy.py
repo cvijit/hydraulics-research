0236ce7cc2e3e0f40484a8fa80351250a70304cc
Vijit Chekkala <55578649+cvijit@users.noreply.github.com>
2020-08-15 01:32:51 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,32 +1,28 @@
-
-
-
 import streamlit as st 
 import numpy as np
-
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
 from sklearn.linear_model import LogisticRegression 
 import xgboost as xgb
 import lightgbm as lgb
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report, f1_score
-
 import bokeh
 import bokeh.layouts
 import bokeh.models
 from bokeh.models.widgets import Div
 
 
+#Link to the dashboard in the web app
 if st.button('EDA Dashboard'):
     js = "window.open('https://hydrauliceda.shinyapps.io/draft1_blank/#section-data-exploration/')"  # New tab or window
+    js = "window.location.href = 'https://hydrauliceda.shinyapps.io/draft1_blank/#section-data-exploration/'"  # Current tab
     html = '<img src onerror="{}">'.format(js)
     div = Div(text=html)
     st.bokeh_chart(div)
 
+#title 
 st.title('Research Project')
 
 st.write("""
@@ -34,6 +30,7 @@ st.write("""
 
 """)
 
+#Choosing the dataset
 dataset_name = st.sidebar.selectbox(
     'Select Dataset',
     ('Coolers', 'Valves', 'Pump_Leakage', 'Hydraulic_Accumulator','Stable_Condition')
@@ -41,12 +38,13 @@ dataset_name = st.sidebar.selectbox(
 
 st.write(f"## {dataset_name} Dataset")
 
+#Choosing the classification algorithm
 classifier_name = st.sidebar.selectbox('Select classifier', 
-                                      ('Logistic Regression','Artificial Neural Network','XGBoost', 'Light GBM','Catboost','Random Forest')
+                                      ('Logistic Regression','Artificial Neural Network','XGBoost', 'Light GBM',
+                                       'Catboost','Random Forest')
 )
 
 #Reading the csv foles from GitHub
-url_data = 'https://github.com/cvijit/hydraulics-research/blob/master/umap_data.csv?raw=true'
 url_1 = 'https://github.com/cvijit/hydraulics-research/blob/master/cooler_deploy.csv?raw=true'
 url_2 = 'https://github.com/cvijit/hydraulics-research/blob/master/valve_deploy.csv?raw=true'
 url_3 = 'https://github.com/cvijit/hydraulics-research/blob/master/pump_deploy.csv?raw=true'
@@ -56,26 +54,27 @@ url_5 = 'https://github.com/cvijit/hydraulics-research/blob/master/stable_deploy
 #Function to read the dataset according to the choosen dataset
 def get_dataset(dataset_name):
     if dataset_name == 'Coolers':
-        data = pd.read_csv(url_data,index_col=0)
+        data = pd.read_csv(url_1,index_col=0)
     elif dataset_name == 'Valves':
-        data = pd.read_csv(url_data,index_col=0)
+        data = pd.read_csv(url_2,index_col=0)
     elif dataset_name == 'Pump_Leakage':
-        data = pd.read_csv(url_data,index_col=0)
+        data = pd.read_csv(url_3,index_col=0)
     elif dataset_name == 'Hydraulic_Accumulator':
-        data = pd.read_csv(url_data,index_col=0)
+        data = pd.read_csv(url_4,index_col=0)
     else:
-        data = pd.read_csv(url_data,index_col=0)
+        data = pd.read_csv(url_5,index_col=0)
     X = data.iloc[:,0:17]
     y= data.iloc[:,-1]
     return X, y
 
 X, y= get_dataset(dataset_name)
 
+#Defining the shape and number of the dataset
 st.write('Shape of dataset', X.shape)
 st.write('Number of classes',len(np.unique(y)))
 
 
-
+#Selecting parameters for classification algorithm
 def add_parameter_ui(clf_name):
     params = dict()
     if clf_name == "Logistic Regression":
@@ -124,31 +123,39 @@ def add_parameter_ui(clf_name):
 
 params = add_parameter_ui(classifier_name)
 
+#Fitting the parameters into the choosen classification model
 def get_classifier(clf_name, params):
     if clf_name == 'Logistic Regression':
-        clf = LogisticRegression(C=params["C"], max_iter=params["max_iter"],solver=params["solver"])
+        clf = LogisticRegression(C=params["C"], max_iter=params["max_iter"],
+                                 solver=params["solver"])
     elif clf_name == 'XGBoost':
-        clf = xgb.sklearn.XGBClassifier(min_child_weight=params['min_child_weight'], max_depth=params['max_depth'], learning_rate=params['learning_rate'], gamma=params['gamma'], colsample_bytree=params['colsample_bytree'])
+        clf = xgb.sklearn.XGBClassifier(min_child_weight=params['min_child_weight'], 
+                                        max_depth=params['max_depth'], learning_rate=params['learning_rate'], 
+                                        gamma=params['gamma'], colsample_bytree=params['colsample_bytree'])
     elif clf_name == 'Light GBM':
         dtrain=lgb.Dataset(X,label=y)
-        clf = lgb.LGBMClassifier(objective=params["objective"],metric=params["metric"],boosting_type=params["boosting_type"],learning_rate=params["learning_rate"],max_depth=params['max_depth'],colsample_bytree=params['colsample_bytree']),dtrain,100
+        clf = lgb.LGBMClassifier(objective=params["objective"],metric=params["metric"],
+                                 boosting_type=params["boosting_type"],learning_rate=params["learning_rate"],
+                                 max_depth=params['max_depth'],colsample_bytree=params['colsample_bytree']),dtrain,100
     else:
-        clf = RandomForestClassifier(bootstrap=params['bootstrap'], criterion=params['criterion'], max_depth=params['max_depth'],max_features=params['max_features'],n_estimators=params['n_estimators'])
+        clf = RandomForestClassifier(bootstrap=params['bootstrap'], criterion=params['criterion'],
+                                     max_depth=params['max_depth'],max_features=params['max_features'],
+                                     n_estimators=params['n_estimators'])
     return clf
 
 clf = get_classifier(classifier_name, params)
 
 
 #### CLASSIFICATION ####
-
+#Splitting the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
+#Fitting the model
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
-
+#Evaluating the model
 acc = accuracy_score(y_test, y_pred)
 cm = confusion_matrix(y_test, y_pred) 
-
+#Displaying Results
 st.write(f'Classifier = {classifier_name}')
 st.write(f'Accuracy =', acc*100)
 st.write(f'Confusion matrix =', cm)
